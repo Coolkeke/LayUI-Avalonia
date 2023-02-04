da135b9ad76a5be80d659b80fba80b9cecdd2492
Coolkeke <37786276+Coolkeke@users.noreply.github.com>
2023-02-04 17:36:43 +0800

    完成选拉框样式

---
+++ src/LayuiAvaloniaPack/LayUI.Avalonia/Controls/ComboBox/LayComboBox.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Generators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LayUI.Avalonia.Controls
+{
+    public class LayComboBox: ComboBox
+    {
+        /// <summary>
+        /// 重写指定下拉选项下拉控件，替换为指定下拉控件
+        /// </summary>
+        /// <returns></returns>
+        protected override IItemContainerGenerator CreateItemContainerGenerator()
+        {
+            return new ItemContainerGenerator<LayComboBoxItem>(
+                this,
+                LayComboBoxItem.ContentProperty,
+                LayComboBoxItem.ContentTemplateProperty);
+        }
+    }
+}
