2475d0e5a26b9d429083f00676b38889cf638704
Coolkeke <37786276+Coolkeke@users.noreply.github.com>
2023-02-07 01:17:02 +0800

    提交代码

---
+++ src/LayuiAvaloniaPack/LayUI.Avalonia/Tools/Keyboard/LayKeyboardHelper.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia.Input.Raw;
+using Avalonia.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Avalonia.Controls; 
+
+namespace LayUI.Avalonia.Tools
+{
+    public class LayKeyboardHelper
+    {
+        public static void SetKey(Key key)
+        {
+            //参考 https://github.com/AvaloniaUI/Avalonia/issues/6775
+            InputManager.Instance.ProcessInput(new RawTextInputEventArgs(KeyboardDevice.Instance, (ulong)DateTime.Now.Ticks, null, ""));
+            InputManager.Instance.ProcessInput(new RawKeyEventArgs(KeyboardDevice.Instance, (ulong)DateTime.Now.Ticks, null, RawKeyEventType.KeyDown, key, RawInputModifiers.None));
+            InputManager.Instance.ProcessInput(new RawKeyEventArgs(KeyboardDevice.Instance, (ulong)DateTime.Now.Ticks, null, RawKeyEventType.KeyUp, key, RawInputModifiers.None));
+        }
+    }
+}
--- src/LayuiAvaloniaPack/Layui.Main/ViewModels/KeyboardPageViewModel.cs
+++ src/LayuiAvaloniaPack/Layui.Main/ViewModels/KeyboardPageViewModel.cs
@@ -11,26 +11,43 @@ using System.Linq;
 using Avalonia.Remote.Protocol.Input;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
+using System.Runtime.InteropServices;
+using Avalonia.Win32;
+using Avalonia.Input.TextInput;
+using Avalonia.Platform;
 
 namespace Layui.Main.ViewModels
 {
     public class KeyboardPageViewModel : ViewModelBase
     {
-        private IKeyboardDevice keyboard = KeyboardDevice.Instance;
-        private IInputManager inputManager = InputManager.Instance;
-        public KeyboardPageViewModel(IContainerExtension container) : base(container) { }
+        public KeyboardPageViewModel(IContainerExtension container) : base(container)
+        {
+
+        }
         private DelegateCommand _KeyCommand;
         public DelegateCommand KeyCommand =>
             _KeyCommand ?? (_KeyCommand = new DelegateCommand(ExecuteKeyCommand));
-
+        private void SetKey(Avalonia.Input.Key key)
+        {
+            var keyboard = KeyboardDevice.Instance;
+            var inputManager = InputManager.Instance;
+            inputManager.ProcessInput(new RawKeyEventArgs(keyboard, (ulong)DateTime.Now.Ticks, null, RawKeyEventType.KeyDown, key, RawInputModifiers.None));
+            inputManager.ProcessInput(new RawKeyEventArgs(keyboard, (ulong)DateTime.Now.Ticks, null, RawKeyEventType.KeyUp, key, RawInputModifiers.None));
+        }
         void ExecuteKeyCommand()
         {
-            var app = Application.Current.ApplicationLifetime as ClassicDesktopStyleApplicationLifetime;
-            var window = app.Windows.Cast<Window>().ToList().Where((o) => o.IsActive)?.FirstOrDefault();
-            if (window != null)
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                //Avalonia.Input.Key.Y 目前尚未找到映射的办法
-                inputManager.ProcessInput(new RawTextInputEventArgs(keyboard, (ulong)DateTime.Now.Ticks, window, "123"));
+                //SetKey(Avalonia.Input.Key);
+                //return "Microsoft YaHei";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                //return "Menlo"; //换成OSX下的中文字体
+            }
+            else
+            {
+                //return "Noto Sans CJK SC";
             }
         }
     }
