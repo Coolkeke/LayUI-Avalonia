6f32cedb712b45dce77ee8a576ee928572c082e6
Coolkeke <37786276+Coolkeke@users.noreply.github.com>
2023-02-21 00:18:42 +0800

    完成单选按钮

---
+++ src/LayuiAvaloniaPack/LayUI.Avalonia/Controls/RadioButton/LayRadioButton.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LayUI.Avalonia.Controls
+{
+    public class LayRadioButton: RadioButton
+    {
+        /// <summary>
+        /// Defines the <see cref="Size"/> property.
+        /// </summary>
+        public static readonly StyledProperty<double> SizeProperty =
+            AvaloniaProperty.Register<LayRadioButton, double>(nameof(Size), 20.0);
+
+        /// <summary>
+        /// Comment
+        /// </summary>
+        public double Size
+        {
+            get { return GetValue(SizeProperty); }
+            set { SetValue(SizeProperty, value); }
+        }
+    }
+}
