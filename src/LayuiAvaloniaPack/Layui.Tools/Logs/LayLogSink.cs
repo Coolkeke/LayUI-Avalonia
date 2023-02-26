7ea1dd925cb6f7ec9937a9391ffa5299664ee8a5
Coolkeke <37786276+Coolkeke@users.noreply.github.com>
2023-02-26 21:49:44 +0800

    测试自定义logger接口

---
+++ src/LayuiAvaloniaPack/Layui.Tools/Logs/LayLogSink.cs
@@ -0,0 +1,42 @@
+﻿using Avalonia.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Layui.Tools.Logs
+{
+    public class LayLogSink : ILogSink
+    {
+        public bool IsEnabled(LogEventLevel level, string area)
+        {
+            return true;
+        }
+
+        public void Log(LogEventLevel level, string area, object? source, string messageTemplate)
+        {
+           
+        }
+
+        public void Log<T0>(LogEventLevel level, string area, object? source, string messageTemplate, T0 propertyValue0)
+        {
+            
+        }
+
+        public void Log<T0, T1>(LogEventLevel level, string area, object? source, string messageTemplate, T0 propertyValue0, T1 propertyValue1)
+        {
+           
+        }
+
+        public void Log<T0, T1, T2>(LogEventLevel level, string area, object? source, string messageTemplate, T0 propertyValue0, T1 propertyValue1, T2 propertyValue2)
+        {
+          
+        }
+
+        public void Log(LogEventLevel level, string area, object? source, string messageTemplate, params object?[] propertyValues)
+        {
+           
+        }
+    }
+}
