a8c0a3464cf534f3538f7035e543ff5a55232d2a
Coolkeke <37786276+Coolkeke@users.noreply.github.com>
2023-03-17 00:10:47 +0800

    调整样式

--- src/LayuiAvaloniaPack/LayUI.Avalonia/Controls/Window/LayWindow.cs
+++ src/LayuiAvaloniaPack/LayUI.Avalonia/Controls/Window/LayWindow.cs
@@ -1,5 +1,7 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Threading;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,10 +9,38 @@ using System.Text;
 namespace LayUI.Avalonia.Controls
 {
     public class LayWindow: Window
-    { 
-        protected override void OnInitialized()
+    {
+
+        /// <summary>
+        /// Defines the <see cref="IsDialog"/> property.
+        /// </summary>
+        public static readonly StyledProperty<bool> IsDialogProperty =
+            AvaloniaProperty.Register<LayWindow, bool>(nameof(IsDialog), false);
+
+        /// <summary>
+        /// 是否为对话框
+        /// </summary>
+        public bool IsDialog
         {
-            base.OnInitialized();
+            get { return GetValue(IsDialogProperty); }
+            set { SetValue(IsDialogProperty, value); }
+        }
+        public override void Show()
+        {
+            base.Show();
+            //判断当前窗体是否为对话框模式
+            if (IsDialog&&WindowStartupLocation == WindowStartupLocation.CenterOwner)
+            {
+                var main = Application.Current.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime;
+                var CenterX = main.MainWindow.Position.X + (main.MainWindow.Bounds.Width / 2);
+                var CenterY = main.MainWindow.Position.Y + (main.MainWindow.Bounds.Height / 2);
+                var X = CenterX - (Bounds.Width / 2);
+                var Y = CenterY - (Bounds.Height / 2);
+                Dispatcher.UIThread.Post(() =>
+                {
+                    Position = new PixelPoint((int)X, (int)Y);
+                });
+            }
         }
     }
 }
--- src/LayuiAvaloniaPack/LayuiApp/Views/Dialog/DialogWindowBase.axaml.cs
+++ src/LayuiAvaloniaPack/LayuiApp/Views/Dialog/DialogWindowBase.axaml.cs
@@ -1,12 +1,13 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
+using LayUI.Avalonia.Controls;
 using Prism.Services.Dialogs;
 using System.Linq;
 
 namespace LayuiApp.Views.Dialog
 {
-    public partial class DialogWindowBase : Window,IDialogWindow
+    public partial class DialogWindowBase : LayWindow, IDialogWindow
     {
         public DialogWindowBase()
         {
