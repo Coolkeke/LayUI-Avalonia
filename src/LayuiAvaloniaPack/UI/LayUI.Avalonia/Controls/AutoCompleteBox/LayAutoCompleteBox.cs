da22daf2e5f0a22281778eb150b2f5eef5cae951
Coolkeke <37786276+Coolkeke@users.noreply.github.com>
2023-05-10 23:01:40 +0800

    完成自动补全输入框样式以及功能

---
+++ src/LayuiAvaloniaPack/UI/LayUI.Avalonia/Controls/AutoCompleteBox/LayAutoCompleteBox.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LayUI.Avalonia.Controls
+{
+    public class LayAutoCompleteBox: AutoCompleteBox
+    {
+        /// <summary>
+        /// 是否聚焦
+        /// </summary>
+        public bool IsFocus
+        {
+            get { return GetValue(IsFocusProperty); }
+            set { SetValue(IsFocusProperty, value); }
+        }
+        /// <summary>
+        /// 定义<see cref="bool"/>属性
+        /// </summary>
+        public static readonly StyledProperty<bool> IsFocusProperty =
+       AvaloniaProperty.Register<LayAutoCompleteBox, bool>(nameof(IsFocus));
+    }
+}
