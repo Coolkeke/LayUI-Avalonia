b891e87bbdf78cbfcadaf33b422ddef7811b8d98
wenke <1357703886@qq.com>
2023-05-22 21:18:30 +0800

    新增动态绑定翻译代码

--- src/LayuiAvaloniaPack/Code/Layui.Tools/Languages/LocalizeBindingExtension.cs
+++ src/LayuiAvaloniaPack/Code/Layui.Tools/Languages/LocalizeBindingExtension.cs
@@ -12,27 +12,64 @@ using System.Threading.Tasks;
 using Avalonia.Controls;
 using System.ComponentModel;
 using System.Drawing;
+using DryIoc;
 
 namespace Layui.Tools.Languages
 {
     /// <summary>
     /// 待测试
     /// </summary>
-    internal class LocalizeBindingExtension
-    {
-        public LocalizeBindingExtension(string path)
-        {
-            Path = path;
-        }
+    public class LocalizeBindingExtension : AvaloniaObject
+    { 
         public LocalizeBindingExtension(IBinding binding)
         {
             Binding = binding;
         }
-        public IBinding Binding { get; set; }
-        public string Path { get; set; }
+        static LocalizeBindingExtension(){
+            BindingProperty.Changed.AddClassHandler<LocalizeBindingExtension>((o, e) => o.OnBindingChanged());
+        }
+
+        private void OnBindingChanged()
+        {
+           
+        }
+        private BindingPriority _priority;
+        /// <summary>
+        /// 这是依赖属性名称
+        /// </summary>
+        public IBinding Binding
+        {
+            get { return GetValue(BindingProperty); }
+            set { SetValue(BindingProperty, value); }
+        }
+        /// <summary>
+        /// 定义<see cref="IBinding"/>属性
+        /// </summary>
+        public static readonly StyledProperty<IBinding> BindingProperty =
+       AvaloniaProperty.Register<LocalizeBindingExtension, IBinding>(nameof(Binding), null);
+
         public object ProvideValue(IServiceProvider serviceProvider)
         { 
-           return Binding;
+            if (Binding is Binding bind)
+            { 
+                if (serviceProvider.GetService(typeof(IProvideValueTarget)) is IProvideValueTarget provideValueTarget &&
+                provideValueTarget.TargetObject is AvaloniaObject targetObject &&
+                provideValueTarget.TargetProperty is AvaloniaProperty targetProperty)
+                {  
+                    if (targetObject.GetValue(targetProperty) is object value)
+                    {
+                        var binding = new ReflectionBindingExtension($"[{value}]")
+                        {
+                            Source = LanguageManager.Instance,
+                            Mode = BindingMode.OneWay,
+
+                        };
+                        return binding.ProvideValue(serviceProvider);
+                    } 
+                }
+                
+            }
+            return AvaloniaProperty.UnsetValue;
         }
     }
 }
