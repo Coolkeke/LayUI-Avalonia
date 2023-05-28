889b516ee52f56cb7d2a35e67befb199ef2cf1c8
Coolkeke <37786276+Coolkeke@users.noreply.github.com>
2023-05-28 20:59:55 +0800

    测试UI效果

--- src/LayuiAvaloniaPack/Apps/BusinessApp.Desktop/MainViewModel.cs
+++ src/LayuiAvaloniaPack/Apps/BusinessApp.Desktop/MainViewModel.cs
@@ -2,6 +2,7 @@
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,12 @@ namespace BusinessApp.Desktop
 {
     public class MainViewModel : BindableBase
     {
+        private ObservableCollection<string> _List=new ObservableCollection<string>();
+        public ObservableCollection<string> List
+        {
+            get { return _List; }
+            set { SetProperty(ref _List, value); }
+        }
         private bool _IsActive;
         public bool IsActive
         {
@@ -26,8 +33,15 @@ namespace BusinessApp.Desktop
         async void ExecuteTestCommand()
         {
            IsActive = true;
-           await Task.Delay(2000);
-           IsActive = false;
+            await Task.Delay(1000);
+            Random ran = new Random();
+            int count = ran.Next(1, 100);
+            List.Clear();
+            for (int i = 0; i < count; i++)
+            {
+                List.Add($"{ran.Next(1, 100)}");
+            }
+            IsActive = false;
         }
     }
 }
