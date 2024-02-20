ee8ee708a0fc8b1516624a5fd3fd62823a273b14
Coolkeke <37786276+Coolkeke@users.noreply.github.com>
2024-02-20 16:40:21 +0800

    完成滚动条样式调整以及数据表格样式调整

---
+++ src/LayuiAvaloniaPack/UI/LayUI.Avalonia/Controls/ScrollBar/LayScrollBar.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Controls.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayUI.Avalonia.Controls
+{
+    public class LayScrollBar: ScrollBar
+    {
+    }
+}
